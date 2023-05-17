c0105cad328fe3233f18ef04bab453de34dd2de9
Drishti <drishtigupta@berkeley.edu>
2023-05-17 14:47:08 -0700

    click for more details button

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles.css';
 import { recipeData } from './recipeData'; // Import the recipeData constant
 import './index.css';
+import { BrowserRouter as Router, Switch, Route, Link, Routes, useParams, useNavigate } from 'react-router-dom';
 
 function App() {
   const [query, setQuery] = useState('');
@@ -27,26 +28,32 @@ function App() {
   }, [query]);
 
   return (
-    <div>
-      <h1 className='header'>My Recipe App</h1>
-      {!searched ? (
-        <SearchBar onSearch={handleSearch} />
-      ) : (
-        <button onClick={handleGoBack}>Go Back</button> //allows you to go back
-      )}
-      <div className="recipe-container">
-        {filteredRecipes.map(recipe => (
-          <RecipeCard
-          key={recipe.title}
-          title={recipe.title}
-          imageUrl={recipe.imageUrl}
-          cuisine={recipe.cuisine}
-          ingredients={recipe.ingredients}
-          instructions={recipe.instructions}
-          />
-        ))}
+    <Router>
+      <div>
+        <h1 className='header'>My Recipe App</h1>
+        {!searched ? (
+          <SearchBar onSearch={handleSearch} />
+        ) : (
+          <button onClick={handleGoBack}>Go Back</button> //allows you to go back
+        )}
+        <div className="recipe-container">
+          {filteredRecipes.map(recipe => (
+            <RecipeCard
+            key={recipe.title}
+            title={recipe.title}
+            imageUrl={recipe.imageUrl}
+            cuisine={recipe.cuisine}
+            ingredients={recipe.ingredients}
+            instructions={recipe.instructions}
+            />
+          ))}
+          <Routes>
+            <Route exact path="/" component={recipeData} />
+            <Route path="/recipes/:recipeTitle" component={RecipeDetailsPage} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
@@ -62,7 +69,7 @@ function SearchBar({ onSearch }) {
   }
 
   return (
-    <div>
+    <div className='search-bar'>
       <input
         type="text"
         placeholder="Search recipes..."
@@ -76,10 +83,15 @@ function SearchBar({ onSearch }) {
 
 function RecipeCard(props) {
   const [expanded, setExpanded] = useState(false);
-
+  const navigate = useNavigate();
   
   function toggleExpand() {
     setExpanded(!expanded);
+  }
+
+  function handleClickForMoreDetails(event) {
+    event.stopPropagation();
+    navigate(`/recipes/${props.title}`);
   }
 
   return (
@@ -97,9 +109,32 @@ function RecipeCard(props) {
           <p>Cuisine: {props.cuisine}</p>
           <p>Ingredients: {props.ingredients}</p>
           <p>Instructions: {props.instructions}</p>
+          <Link to={`/recipes/${props.title}`}>
+            <button onClick={handleClickForMoreDetails}>Click for more details</button>
+          </Link>
           <VoteButton />
         </div>
       )}
+    </div>
+  );
+}
+
+function RecipeDetailsPage() {
+  const { recipeTitle } = useParams();
+  const recipe = recipeData.find(recipe => recipe.title === recipeTitle);
+
+  if (!recipe) {
+    return <div>Recipe not found</div>;
+  }
+
+  return (
+    <div>
+      <h2>{recipe.title}</h2>
+      <p>Cuisine: {recipe.cuisine}</p>
+      <p>Ingredients: {recipe.ingredients}</p>
+      <p>Instructions: {recipe.instructions}</p>
+      <p>Cooking Time: {recipe.cooking_time}</p>
+      <p>Serving Size: {recipe.serving_size}</p>
     </div>
   );
 }
